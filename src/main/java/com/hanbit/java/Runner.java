8af37fc2ae48f52c0a7eba844898077668af758f
11 <11@hb5012>
2016-12-07 18:28:02 +0900

    runner 수정

--- src/main/java/com/hanbit/java/Runner.java
+++ src/main/java/com/hanbit/java/Runner.java
@@ -12,32 +12,28 @@ import com.hanbit.animal.Dog;
 import com.hanbit.animal.Hamster;
 
 public class Runner {
-	
+
 	public static void main(String[] args) {
 		Livable dog = new Dog("도베르만", "검정");
 		Livable cat = new Cat("길고양이", "줄무늬");
 		Livable hamster = new Hamster("모름", "줄무늬");
-		
+
 		List list = new ArrayList();
 		Map map = new HashMap();
-		
-		Livable tree = () -> { return true; };
-		
+
 		useType(dog);
 		useType(cat);
 		useType(hamster);
 	}
-	
+
 	private static void useType(Livable livable) {
 		if (livable instanceof Cat) {
 			Cat cat = (Cat) livable;
 			cat.climbTree();
-		}
-		else if (livable instanceof Breathable) {
+		} else if (livable instanceof Breathable) {
 			Breathable breathable = (Breathable) livable;
 			breathable.breathe();
 		}
 	}
-	
-	System.out.println(livable.getClass().getSimpleName());
+
 }
